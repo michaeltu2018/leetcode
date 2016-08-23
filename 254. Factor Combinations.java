b5b5316ee83525b5fbe7368733bf2b903cc5aa56
michaeltu2018 <long.tu.12fall@gmail.com>
2016-08-22 17:39:59 -0700

    Create 254. Factor Combinations.java

---
+++ 254. Factor Combinations.java
@@ -0,0 +1,31 @@
+public class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> getFactors(int n) {
+        helper(new ArrayList<Integer>(), n, 2);
+        return res;
+    }
+    
+    void helper(List<Integer> list, int n, int cur){
+        if(n == 1){
+            if(list.size()>1){
+                res.add(new ArrayList<Integer>(list));
+            }
+            return;
+        }
+        for(int i=cur;i<=Math.sqrt(n);i++){
+            if(list.size() >= 1 && list.get(list.size()-1) > i){
+                continue;
+            }
+            if(n%i==0){
+                list.add(i);
+                helper(list,n/i,i);
+                list.remove(list.size()-1);
+            }
+        }
+        int i = n; 
+        list.add(i);
+        helper(list,n/i,i);
+        list.remove(list.size()-1);
+    }
+}
