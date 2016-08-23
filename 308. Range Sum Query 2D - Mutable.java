ce03203b9afd3bac456560275591e247586b46cb
michaeltu2018 <long.tu.12fall@gmail.com>
2016-08-22 17:00:00 -0700

    Create 308. Range Sum Query 2D - Mutable.java

---
+++ 308. Range Sum Query 2D - Mutable.java
@@ -0,0 +1,57 @@
+public class NumMatrix {
+    private int[][] nums;
+    int m;
+    int n;
+    int[][] tree;
+    public NumMatrix(int[][] matrix) {
+        if(   matrix           == null
+           || matrix.length    == 0
+           || matrix[0].length == 0   ){
+            return;   
+         }
+         
+         this.m   = matrix.length;
+         this.n   = matrix[0].length;
+         this.nums = new int[m][n];
+         this.tree = new int[m+1][n+1];
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 update(i,j,matrix[i][j]);
+             }
+         }
+    }
+
+    public void update(int row, int col, int val) {
+        if (m == 0 || n == 0) return;
+        int delta = val - nums[row][col];
+        nums[row][col] = val;
+        for(int i=row+1;i<=m;i+=(i&-i)){
+            for(int j=col+1;j<=n;j+=(j&-j)){
+                tree[i][j] += delta;
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        return sum(row2+1,col2+1) + sum(row1,col1) - sum(row1, col2+1) - sum(row2+1,col1);
+    }
+    
+    int sum(int row, int col){
+        int sum = 0;
+        for(int i=row;i>0;i-=(i&-i)){
+             for(int j=col;j>0;j-=(j&-j)){
+                 sum += tree[i][j];
+             }
+         }
+         return sum;
+    }
+    
+}
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.update(1, 1, 10);
+// numMatrix.sumRegion(1, 2, 3, 4);
