8ac557e9f445a55e7bd63e2a74e8d96e31720c75
michaeltu2018 <long.tu.12fall@gmail.com>
2016-12-11 14:00:25 -0800

    Update 320. Generalized Abbreviation.java

--- 320. Generalized Abbreviation.java
+++ 320. Generalized Abbreviation.java
@@ -14,3 +14,32 @@ public class Solution {
     }
 }
 
+
+// add cache to speed up: 30 ms
+public class Solution {
+    Map<String, List<String>> map = new HashMap<>();
+    public List<String> generateAbbreviations(String word) {
+        if(map.containsKey(word)){
+            return map.get(word);
+        }
+        List<String> res = new ArrayList<>();
+        int len = word.length();
+        res.add(len==0 ? "" : String.valueOf(len));
+        for(int i=0;i<len;i++){
+            List<String> rights;
+            if(map.containsKey(word.substring(i+1))){
+                rights = map.get(word.substring(i+1));
+            }
+            else{
+                rights = generateAbbreviations(word.substring(i+1));
+            }
+            
+            for(String s : rights){
+                String leftNum = i>0?Integer.toString(i):"";
+                res.add(leftNum + word.substring(i,i+1) + s);
+            }
+        }
+        map.put(word, res);
+        return res;
+    }
+}
