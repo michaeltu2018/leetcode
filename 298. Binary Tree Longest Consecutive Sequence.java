08e73ad0232453d44b9898067a300f2d95284b17
michaeltu2018 <long.tu.12fall@gmail.com>
2017-03-16 20:38:46 -0700

    Update 298. Binary Tree Longest Consecutive Sequence.java

--- 298. Binary Tree Longest Consecutive Sequence.java
+++ 298. Binary Tree Longest Consecutive Sequence.java
@@ -32,3 +32,31 @@ public class Solution {
         helper(root.right, root.val+1, cur);
     }
 }
+
+
+public class Solution {
+    //int result = 0;
+    public int longestConsecutive(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return helper(root,root.val,0);
+        //return result;
+    }
+    
+    int helper(TreeNode root, int target, int cur){
+        if(root == null){
+            return;
+        }
+        if(root.val == target){
+            cur++;
+            //result = Math.max(cur, result);
+        }
+        else{
+            cur = 1;
+        }
+        int l = helper(root.left, root.val+1, cur);
+        int r = helper(root.right, root.val+1, cur);
+        return Math.max(cur, Math.max(l, r));
+    }
+}
