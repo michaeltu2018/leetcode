f3bd50e5450598c89ac419e3fe4fb3eba5b673a2
mtu20181234 <long.tu.12fall@gmail.com>
2018-05-28 20:26:58 -0700

    Update 272. Closest Binary Search Tree Value II.java

--- 272. Closest Binary Search Tree Value II.java
+++ 272. Closest Binary Search Tree Value II.java
@@ -7,6 +7,9 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+time: O(n)
+space: O(k)
+
 public class Solution {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> list = new LinkedList<Integer>();
@@ -27,4 +30,82 @@ public class Solution {
         helper(list, root.right, target, k);
     }
     
+}
+
+
+
+Analysis:
+The time complexity would be O(k + k * logk). 
+Space complexity is O(k).
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+         
+        Stack<Integer> precedessor = new Stack<>();
+        Stack<Integer> successor = new Stack<>();
+         
+        getPredecessor(root, target, precedessor, k);
+        getSuccessor(root, target, successor, k);
+         
+        for (int i = 0; i < k; i++) {
+            if (precedessor.isEmpty()) {
+                result.add(successor.pop());
+            } else if (successor.isEmpty()) {
+                result.add(precedessor.pop());
+            } else if (Math.abs((double) precedessor.peek() - target) < Math.abs((double) successor.peek() - target)) {
+                result.add(precedessor.pop());
+            } else {
+                result.add(successor.pop());
+            }
+        }
+         
+        return result;
+    }
+     
+    //time: O(k)
+    private void getPredecessor(TreeNode root, double target, Stack<Integer> precedessor, int k) {
+        if (root == null) {
+            return;
+        }
+         
+        getPredecessor(root.left, target, precedessor, k);
+         
+        if (root.val > target || precedessor.size() >= k) {
+            return;
+        }
+         
+        precedessor.push(root.val);
+         
+        getPredecessor(root.right, target, precedessor, k);
+    }
+     
+    //time: O(k)
+    private void getSuccessor(TreeNode root, double target, Stack<Integer> successor, int k) {
+        if (root == null) {
+            return;
+        }
+         
+        getSuccessor(root.right, target, successor, k);
+         
+        if (root.val <= target || successor.size() >= k) {
+            return;
+        }
+         
+        successor.push(root.val);
+         
+        getSuccessor(root.left, target, successor, k);
+    }
 }
